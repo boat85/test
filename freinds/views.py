c8269f7ba96a5aad216008e9013e307f0956f06c
Boat <tawankaewrod@gmail.com>
2022-06-30 23:24:35 +0700

    frist save

---
+++ freinds/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def showdata(request):
+  return HttpResponse('Hi my friends')
+
+
+def showdatafs(re):
+  return HttpResponse('I have not my freinds ')
+
